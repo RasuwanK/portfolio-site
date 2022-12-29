06fd7f4ae7018eefb515c53bdbc214c4e13a65ef
Rasuwan Kalhara <kalharaweragala@gmail.com>
2022-12-30 01:19:34 +0530

    Removed the event listener on window

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,31 +6,25 @@ import { useEffect } from 'react'
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     console.log('Hi');
-    const themeCallback = () => {
-      console.log("loaded !");
-      // Obtaining theme value from the localstorage
-      const theme = localStorage.getItem("theme");
-      if (theme) {
-        // If theme is available in the local storage then use it
-        document.documentElement.classList.add(theme);
+    console.log("loaded !");
+    // Obtaining theme value from the localstorage
+    const theme = localStorage.getItem("theme");
+    console.log(theme);
+    if (theme) {
+      console.log("theme is not null");
+      // If theme is available in the local storage then use it
+      document.documentElement.classList.add(theme);
+    } else {
+      console.log("theme is null");
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        console.log("query matched");
+        // If not awailable then prefer system theme preference
+        localStorage.setItem("theme", "dark");
+        document.documentElement.classList.add("dark");
       } else {
-        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-          // If not awailable then prefer system theme preference
-          localStorage.setItem("theme", "dark");
-          document.documentElement.classList.add("dark");
-        } else {
-          localStorage.setItem("theme", "light");
-          document.documentElement.classList.add("light");
-        }
+        localStorage.setItem("theme", "light");
+        document.documentElement.classList.add("light");
       }
-    }
-
-    // Theme is applied after the body is loaded
-    window.addEventListener("load", themeCallback);
-
-    return () => {
-      // Cleaning the event listener
-      window.removeEventListener("load", themeCallback);
     }
   }, []);
 
