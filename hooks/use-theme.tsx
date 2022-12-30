0cce34cddff42076174e2116c0d5eaa982a18540
Rasuwan Kalhara <kalharaweragala@gmail.com>
2022-12-30 20:02:16 +0530

    Redesigned use theme hook

--- hooks/use-theme.tsx
+++ hooks/use-theme.tsx
@@ -1,34 +1,49 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 
 type ThemeChoice = "dark" | "light";
 
+type State = {
+    theme: ThemeChoice | undefined;
+}
+
+type Action = {
+    choice: ThemeChoice;
+}
+
+type Reducer = (state:State, action: Action) => State;
+
+function reducer(state: State, action: Action) {
+    const documentClassList = document.documentElement.classList;
+    let value: State = {theme: undefined};
+    if (action.choice === "dark") {
+        documentClassList.remove("light");
+        documentClassList.add("dark");
+        localStorage.setItem("theme", "dark");
+        value.theme = "dark";
+    } else {
+        documentClassList.remove("dark");
+        documentClassList.add("light");
+        localStorage.setItem("theme", "light");
+        value.theme = "light";
+    }
+
+    return value;
+}
+
 // To use this hook theme must be initialized at _app
 export default function useTheme() {
-    const [theme, setTheme] = useState<ThemeChoice | undefined>(undefined);
+    const [{theme}, dispatch] = useReducer<Reducer>(reducer, { theme: undefined });
     useEffect(() => {
         const theme = document.documentElement.classList.contains("dark");
         if (theme) {
-            setTheme("dark");
+            dispatch({choice: "dark"});
         } else {
-            setTheme("light");
+            dispatch({choice: "light"});
         }
     }, []);
 
     return {
         theme,
-        setTheme: (isDark: boolean) => {
-            const documentClassList = document.documentElement.classList;
-            if (isDark) {
-                setTheme("dark");
-                documentClassList.remove("light");
-                documentClassList.add("dark");
-                localStorage.setItem("theme", "dark");
-            } else {
-                setTheme("light");
-                documentClassList.remove("dark");
-                documentClassList.add("light");
-                localStorage.setItem("theme", "light");
-            }
-        },
+        setTheme: (theme: ThemeChoice) => dispatch({choice: theme})
     }
 }
