f0ba663ce79e801d79b23b419cc153e290f4638a
Rasuwan Kalhara <kalharaweragala@gmail.com>
2022-12-31 20:40:57 +0530

    Added a footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -14,17 +14,18 @@ const poppinsFont = Poppins({
 type ThemeChoice = "dark" | "light";
 
 export default function Layout({ children }: LayoutProps) {
-
     return (
         // Holding the entire page layout (each page shares this layout)
         <div id="layout" className={`${poppinsFont.className}`}>
             <header id="header-content" className="z-10 fixed top-0 w-full h-[80px]">
                 <Navbar />
             </header>
-            <div id="page-holder" className="mt-[80px] z-0 bg-light-white dark:bg-dark-black">
+            <div id="page-holder" className="mt-[80px] pb-10 z-0 bg-light-white dark:bg-dark-black">
                 {children}
             </div>
-            <footer id="footer-content"></footer>
+            <footer id="footer-content" className="py-10 bg-dark-teal dark:bg-light-black p-4 flex flex-col items-center text-dark-black dark:text-light-white">
+                <span>Copyright (C) Rasuwan Kalhara</span>
+            </footer>
         </div>
     )
 }
